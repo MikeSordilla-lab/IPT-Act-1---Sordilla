933981d45209b50c6dc0fc359c811ac87ead533c
MikeSordilla <mike.sordilla@nmsc.edu.ph>
2026-04-07 09:04:11 +0800

    style: Format vite.config.js for consistency and readability

--- my-web-app/vite.config.js
+++ my-web-app/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-const isUserOrOrgPage = repositoryName.endsWith('.github.io')
-const base = process.env.BASE_PATH ?? (process.env.GITHUB_REPOSITORY ? (isUserOrOrgPage ? '/' : `/${repositoryName}/`) : '/')
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isUserOrOrgPage = repositoryName.endsWith(".github.io");
+const base =
+  process.env.BASE_PATH ??
+  (process.env.GITHUB_REPOSITORY
+    ? isUserOrOrgPage
+      ? "/"
+      : `/${repositoryName}/`
+    : "/");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base,
-})
+});
